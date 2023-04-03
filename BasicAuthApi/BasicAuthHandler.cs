9492793fe81eb8be3ff4525a28b531201c7007f9
Chris Higgi <chrishiggi@gmail.com>
2023-04-03 16:03:07 +0000

    added constants to basic auth handler

--- BasicAuthApi/BasicAuthHandler.cs
+++ BasicAuthApi/BasicAuthHandler.cs
@@ -2,6 +2,11 @@ using System.Text;
 
 public class BasicAuthHandler
 {
+    private const int UserIdIndex = 0;
+    private const int PasswordIndex = 1;
+    private const string Separator = ":";
+    private const string UnauthorizedText = "Unauthorized...";
+    private const string AuthorizationHeaderKey = "Authorization";
     private readonly RequestDelegate next;
     private readonly string relm;
 
@@ -13,25 +18,25 @@ public class BasicAuthHandler
 
     public async Task InvokeAsync(HttpContext context)
     {
-        if (!context.Request.Headers.ContainsKey("Authorization"))
+        if (!context.Request.Headers.ContainsKey(AuthorizationHeaderKey))
         {
-            context.Response.StatusCode = 401;
-            await context.Response.WriteAsync("Unauthorized...");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync(UnauthorizedText);
             return;
         }
 
         // Basic userId:password
-        var header = context.Request.Headers["Authorization"].ToString();
+        var header = context.Request.Headers[AuthorizationHeaderKey].ToString();
         var encodedCreds = header.Substring(6);
         var creds = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCreds));
-        string[] uidpw = creds.Split(":");
-        var uid = uidpw[0];
-        var pw = uidpw[1];
+        string[] uidpw = creds.Split(Separator);
+        var uid = uidpw[UserIdIndex];
+        var pw = uidpw[PasswordIndex];
 
-        if (uid != "MyUser" && pw != "MyPassword")
+        if (uid != "MyUser" || pw != "MyPassword")
         {
-            context.Response.StatusCode = 401;
-            await context.Response.WriteAsync("Unauthorized...");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync(UnauthorizedText);
             return;
         }
 
