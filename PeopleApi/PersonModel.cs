5ff1a6f629c40d5cf6207faf13f463c628645364
Chris Higgi <chrishiggi@gmail.com>
2023-06-06 04:25:47 +0000

    Added simple PeopleApi Project to try out http doc

---
+++ PeopleApi/PersonModel.cs
@@ -0,0 +1,5 @@
+public class PersonModel
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ PeopleApi/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+List<PersonModel> people = new()
+{
+    new() { Id = 1, Name = "Moe" },
+    new() { Id = 2, Name = "Larry" },
+    new() { Id = 3, Name = "Curly" },
+};
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/People", () => people);
+app.MapPost("/People", (PersonModel p) => people.Add(p));
+app.MapPut("/People/{id}", (int id, PersonModel p) =>
+{
+    var person = people.Where(x => x.Id == id).FirstOrDefault();
+    if (person is not null)
+    {
+        person.Name = p.Name;
+    }
+
+    return person;
+});
+app.MapDelete("/People/{id}", (int id) => people.Remove(people.Find(x => x.Id == id))!);
+
+app.Run();
